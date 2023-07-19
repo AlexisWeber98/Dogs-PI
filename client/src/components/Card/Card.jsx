812ed76a84778eb5db8dc865a9b91a31b1e204eb
Alexis Weber <Alexis.Weber@Mailfence.com>
2023-07-19 10:08:26 -0300

    cards details margin correction

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,9 +7,9 @@ const Card = ({id,name, bred_for, reed_group, life_span, temperament, origin, im
         <div className='card'>
             <NavLink className="navLink" to={`/detail/${id}`}>
                 <img className='img' src={image} alt={name} />
-                <h2>Name | {name}</h2>
-                {weight && <h3>Weight | {weight.metric?weight.metric: weight}</h3>}
-                <h3>Temperament | {temperament} </h3>
+                <h2 className="h2-nameDog">Name | {name}</h2>
+                {weight && <h3 className="h3-dogcardDetail">Weight | {weight.metric?weight.metric: weight}</h3>}
+                <h3 className="h3-dogcardDetail">Temperament | {temperament} </h3>
             </NavLink>
 
         </div>
