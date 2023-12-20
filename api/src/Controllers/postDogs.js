9eb43941682bbc35bc8b23192bbf1d51dcd5474c
Alexis Weber <alexis.weber@mailfence.com>
2023-12-19 21:01:13 -0300

    change error handle

--- api/src/Controllers/postDogs.js
+++ api/src/Controllers/postDogs.js
@@ -58,6 +58,7 @@ module.exports = postDogs = async (req, res) => {
 
     return res.status(200).json(dog);
   } catch (error) {
-    res.status(500).send(error.message);
+    console.error(error);
+    res.status(500).send(error);
   }
 };
