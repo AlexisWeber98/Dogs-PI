ec28d53ad54f2c6d0250e1bc0644f37753fd5d1f
Alexis Weber <alexis.weber@mailfence.com>
2024-01-10 22:09:15 -0300

    add useEffect and add dogs on landing page

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,13 +1,18 @@
 import { NavLink } from "react-router-dom";
-import './Landing.css'
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import "./Landing.css";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import WoD from "../../../public/WoD.png";
-
+import { getAllDogs } from "../../redux/action";
 
 const Landing = () => {
   const dogs = useSelector((state) => state.filteredDogs);
   const [dogId, setDogId] = useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllDogs());
+  }, [dogs]);
 
   const handleNextDog = () => {
     if (dogId < dogs.length - 1) {
@@ -22,45 +27,54 @@ const Landing = () => {
   };
 
   return (
-    
-  <div className="landingContainer">
-    <div className="title">
-    <img className="logo" src={WoD} alt="logo" width={150} />
-      <h1>Welcome to World Of Dogs!</h1>
-    </div>
-    <hr />
-    <br />
-    {dogs.length > 0 && (
-      <>
-        <article className="block">
-          <div className="buttonContainer">
-            <button onClick={handlePrevDog} disabled={dogId === 0}>Previous</button>
-          </div>
+    <div className="landingContainer">
+      <div className="title">
+        <img className="logo" src={WoD} alt="logo" width={150} />
+        <h1>Welcome to World Of Dogs!</h1>
+      </div>
+      <hr />
+      <br />
+      {dogs.length > 0 && (
+        <>
+          <article className="block">
+            <div className="buttonContainer">
+              <button onClick={handlePrevDog} disabled={dogId === 0}>
+                Previous
+              </button>
+            </div>
 
-          <div  className="imageLandingContainer" >
-            <img className="imageLanding" src={dogs[dogId].image} alt={dogs[dogId].name} />
+            <div className="imageLandingContainer">
+              <img
+                className="imageLanding"
+                src={dogs[dogId].image}
+                alt={dogs[dogId].name}
+              />
             </div>
             <div className="buttonContainer">
-              <button onClick={handleNextDog} disabled={dogId === dogs.length - 1}>Next</button>
+              <button
+                onClick={handleNextDog}
+                disabled={dogId === dogs.length - 1}>
+                Next
+              </button>
             </div>
-                
-        </article>
+          </article>
           <div className="finalBlock">
-          <div className="nameCont">
-            <h3 className="nameLanding" >Name: {dogs[dogId].name}</h3>
-          </div>
-            <hr /> 
+            <div className="nameCont">
+              <h3 className="nameLanding">Name: {dogs[dogId].name}</h3>
+            </div>
+            <hr />
 
-          <div className="navLinkButton">
-            <NavLink className="buttonEnter" to="/home">Enter to the Page</NavLink>
-          </div>
-          <hr />
+            <div className="navLinkButton">
+              <NavLink className="buttonEnter" to="/home">
+                Enter to the Page
+              </NavLink>
+            </div>
+            <hr />
           </div>
         </>
       )}
     </div>
   );
-      
 };
 
 export default Landing;
